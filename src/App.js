bd44eeca98ad289e565967c884eb0f69077ba41e
Rahad Khan <clash146098@gmail.com>
2022-04-17 00:53:38 +0600

    Environment created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header/Header';
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/Home/Home';
+import Blog from './components/Blog/Blog';
+import CheckOut from './components/CheckOut/CheckOut';
+import AboutMe from './components/AboutMe/AboutMe';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/blog' element={<Blog></Blog>}></Route>
+        <Route path='/aboutme' element={<AboutMe/>}></Route>
+        <Route path='/checkout' element={<CheckOut/>}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import CustomLink from "../CustomLink/CustomLink";
+import { CgMenu } from "react-icons/cg";
+import { ImCross } from "react-icons/im";
+
+const Header = () => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <header  className='md:w-5/6 mx-auto'>
+      <nav className="bg-white border-gray-200 sm:py-2.5 rounded">
+        <div className="container flex flex-wrap justify-between items-center mx-auto">
+        <Link to="/" className="flex items-center">
+            <p className="text-4xl font-semibold">Medi 
+              <span className="whitespace-nowrap text-green-400">
+                Line
+              </span>
+            </p>
+          </Link>
+          <button onClick={() => setOpen(!open)}
+          className="block md:hidden"
+          >
+            {
+              open ? <ImCross/> : <CgMenu className="text-2xl font-extraboldbold"/>
+            }
+          </button>
+          <div className={`w-full md:block md:w-auto ${open ? 'block' : 'hidden'}`}>
+            <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-lg md:font-medium">
+              <li className="mb-2">
+                <CustomLink
+                  to="/"
+                >
+                  Home
+                </CustomLink>
+              </li>
+              <li className="mb-2">
+                <CustomLink
+                  to="/blog">
+                  Blog
+                </CustomLink>
+              </li>
+              <li className="mb-2">
+                <CustomLink
+                  to="/aboutme">
+                  About Me
+                </CustomLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
